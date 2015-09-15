cb5c58f7492569a0ba0c90b49012724dd8b89915
David G <dgabbe@acm.org>
2015-09-14 21:09:21 -0400

    machine specific file is appended to the scripts list if found.

--- mk_sym_links.sh
+++ mk_sym_links.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 sdir=$(dirname ${BASH_SOURCE})
-scripts=("bash_aliases" "bash_logout" "bash_profile" "bashrc"  "dgabbeMini_profile" "profile")
+
+scripts=("bash_aliases" "bash_logout" "bash_profile" "bashrc" "profile")
+
+# Append machine specific file if found
+my_machine=${HOSTNAME%.local}_profile
+if [ -e "${my_machine}" ]; then
+	scripts+=(${my_machine})
+fi
+
+echo ${scripts[@]}
+
 for s in "${scripts[@]}"
 do
 	f=$HOME/.${s}
