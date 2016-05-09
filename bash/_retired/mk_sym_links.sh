4a7a36921a61c02eb2a53638bfa286267cc5e4b1
David G <dgabbe@acm.org>
2016-05-09 13:01:31 -0400

    Moved performance tweaks to home-bin, it's not a profile.  Switched from bash to python install scripts.

--- bash/_retired/mk_sym_links.sh
+++ bash/_retired/mk_sym_links.sh
@@ -2,7 +2,7 @@
 
 sdir=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd -P)
 
-scripts=("bash_aliases" "bash_logout" "bash_profile" "bashrc" "profile" "Xdefaults")
+scripts=("bash_aliases" "bash_logout" "bash_profile" "bashrc" "profile")
 
 # Append machine specific file if found
 my_machine=${HOSTNAME%.local}_profile
