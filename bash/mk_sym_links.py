e9d9a4b00f699f5088866bd2d8c4b2fd0e3ed39a
David G <dgabbe@acm.org>
2016-11-14 09:23:00 -0500

    Add laptop specific file; remove extra space in installer message.

--- bash/mk_sym_links.py
+++ bash/mk_sym_links.py
@@ -6,7 +6,7 @@
 #
 # Repo file names are with the leading "."
 #
-# Assume if a link is found, the environment is already setup, but not 
+# Assume if a link is found, the environment is already setup, but not
 # necessarily by this repo.
 #
 
@@ -27,13 +27,13 @@ if os.path.exists(my_machine):
 
 for s in scripts:
 	f = os.path.join(home, "." + s)
-	
+
 	if os.path.isfile(f) and not os.path.islink(f):
 		os.rename(f, f + ".org")
-		print "\n    Moved ", f, " to ", f, ".org"
+		print "\n    Moved ", f, "to ", f, ".org"
 
 	if os.path.islink(f):
-		print"\n    ", f, " is a sym link. No changes to make."
+		print"\n    ", f, "is a sym link. No changes to make."
 	else:
 		os.symlink(os.path.join(repo, s), f)
 		print "\n    ", f, " -> ", os.path.realpath(f)
