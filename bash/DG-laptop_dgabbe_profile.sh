26a4fea6908445c658b744a6b0c9a59651ed2d5b
David G <dgabbe@acm.org>
2018-12-29 10:34:37 -0500

    Added gimp alias; created new profile to match new laptop;

---
+++ bash/DG-laptop_dgabbe_profile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#
+# Setting CDPATH for systems w/multiple partitions or disks is handy
+# Reminder: Set path to be parent of the directory you want
+#
+export CDPATH=:${HOME}:${HOME}/_git/
+export HOMEBREW_CACHE=/Groups/Homebrew/Caches/
+export PYTHONPATH=${HOME}/_git/_python/
